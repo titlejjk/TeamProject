8bda1abb4cdf5143fbbf1df32d437b966460d372
jojungeun <title.jjk@gmail.com>
2023-08-25 11:21:38 +0900

    JWT Token 추출 및 Http Header에 담기 메서드 완성

--- src/main/java/com/project/project/user/controller/AuthController.java
+++ src/main/java/com/project/project/user/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.project.project.user.controller;
 
+import com.project.project.security.TokenProvider;
 import com.project.project.user.dto.SignInResponseDto;
 import com.project.project.user.dto.SignUpRequest;
 import com.project.project.user.dto.UserDto;
@@ -25,6 +26,7 @@ public class AuthController {
     private final AuthServiceImpl authService;
     //TokenBlacklist
     private final Set<String> tokenBlacklist = Collections.synchronizedSet(new HashSet<>());
+    private final TokenProvider tokenProvider;
 
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
@@ -40,14 +42,15 @@ public class AuthController {
 
     //로그인
     @PostMapping("/signin")
-    public ResponseEntity<?>signIn(@RequestBody UserDto request){
-        SignInResponseDto response = authService.authenticateUser(
+    public ResponseEntity<?>signIn(@RequestBody UserDto request, HttpServletResponse response){
+        SignInResponseDto signInResponseDto = authService.authenticateUser(
                 request.getUserEmail(),
                 request.getUserPassword()
-
         );
-        System.out.println(request);
-        return ResponseEntity.ok(response);
+        //Authorization 헤더에 토큰 설정
+        response.setHeader("Authorization", "Bearer " + signInResponseDto.getToken());
+        System.out.println(response);
+        return ResponseEntity.ok(signInResponseDto);
     }
 
     //회원가입시 이메일 중복 체크
@@ -66,4 +69,18 @@ public class AuthController {
     }
     //인증 필터나 인터셉터에서 블랙리스트확인
     //블랙리스트에 있는 토큰을 사용한 요청은 거부
+
+    //token 검증
+    @GetMapping("/validate-token")
+    public ResponseEntity<String> validateToken(@RequestHeader("Authorization") String authHeader){
+        String token = authHeader.substring(7);//"Bearer "삭제하고 token값으로 넣기
+        try{
+            String userEmail = tokenProvider.validate(token);
+
+            return ResponseEntity.ok("Token is valid. User Email : " + userEmail);
+        }catch (Exception e){
+            //토큰검증 실패
+            return ResponseEntity.badRequest().body("Invalid token");
+        }
+    }
 }
