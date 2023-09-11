5283115be11d7d10211357630ef046c991a668f8
jojungeun <title.jjk@gmail.com>
2023-09-11 10:59:39 +0900

    TemaProject BackEnd

--- src/main/java/com/project/user/controller/AuthController.java
+++ src/main/java/com/project/user/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.project.user.dto.SignInResponseDto;
 import com.project.user.dto.SignUpRequest;
 import com.project.user.dto.UserDto;
 import com.project.user.service.AuthServiceImpl;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
@@ -17,7 +18,7 @@ import java.util.Set;
 
 @CrossOrigin(originPatterns = "http://localhost:3000")
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
     //AuthServiceImpl 의존성
@@ -27,6 +28,7 @@ public class AuthController {
     private final TokenProvider tokenProvider;
 
     //회원가입
+    @ApiOperation(value = "회원 가입")
     @PostMapping
     public ResponseEntity<?>registerUser(@RequestBody SignUpRequest request){
         authService.signUp(request);
